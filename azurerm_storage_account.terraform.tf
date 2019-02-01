cf07c99be0af6b5ecade689485adcb27f0da79c7
James Woolfenden <james.woolfenden@gmail.com>
2019-02-01 22:55:41 +0000

    first draft

--- azurerm_storage_account.terraform.tf
+++ azurerm_storage_account.terraform.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "terraform" {
-  name                     = "terraformstate"
+  name                     = "terraform${element(split("-", "ee53c671-d145-4a84-a694-e80ef1f6edd1"),0)}"
   resource_group_name      = "${azurerm_resource_group.terraform.name}"
   location                 = "${var.location}"
   account_tier             = "Standard"
--- data.template_file.remote_state.tf
+++ data.template_file.remote_state.tf
@@ -1,5 +1,7 @@
 data "template_file" "remote_state" {
   template = "${file("${path.module}/remote_state.tf.template")}"
 
-  vars {}
+  vars {
+    storage_account_name = "terraform${element(split("-", "ee53c671-d145-4a84-a694-e80ef1f6edd1"),0)}"
+  }
 }
